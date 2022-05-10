8cebf068ccaf6b1776325311cce24c420c2110d0
Michael Pace <michael.g.pace@gmail.com>
2022-05-10 16:19:59 -0500

    deleted unused code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@
  
  class App extends React.Component{
  
-
    /**
     * @returns React.Component
     */
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -9,21 +9,15 @@ import { Image } from 'react-bootstrap'
 class HornedBeast extends React.Component{
   render(){
     return(
-
       <section>
             <h2>{this.props.beastObs.title}</h2>
               <Image
               src={this.props.beastObs.image_url}
               />
-              
-              {/* < */}
-            {/* <img src={this.props.src} alt={this.props.alt} title={this.props.title} /> */}
             <p>{this.props.beastObs.description}</p>
       </section>
     );
   }
-
-
 }
 
 
